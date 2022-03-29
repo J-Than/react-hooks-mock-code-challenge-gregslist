a8477d3689e2e04dcb0b6e81970475b64c4e9948
Jarod Wilson <jaroddwilson@gmail.com>
2022-03-29 18:57:05 -0400

    First deliverable (see all listings) completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+
+  const [listings, setListings] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:6001/listings")
+    .then(r => r.json())
+    .then(listReturn => setListings(listReturn))
+  }, [])
+
+  const filteredList = listings
+
   return (
     <div className="app">
       <Header />
-      <ListingsContainer />
+      <ListingsContainer listings={filteredList} />
     </div>
   );
 }
